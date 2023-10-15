66c771da4682d898c7271ea53859b603d592cf69
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-15 12:24:46 -0400

    Add project files.

---
+++ femtosecond/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Xaml.Media;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace femtosecond
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private AppWindow m_AppWindow;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            m_AppWindow = GetAppWindowForCurrentWindow();
+
+            if(AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = m_AppWindow.TitleBar;
+                titleBar.ExtendsContentIntoTitleBar = true;
+                ExtendsContentIntoTitleBar = true;
+            } 
+            else
+            {
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
+        }
+        
+        private AppWindow GetAppWindowForCurrentWindow() 
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
+        }
+    }
+}
