abf9c2f7e1e4e9c1d1d221221ec02500e157e36d
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-16 22:17:24 -0400

    use MVVM to handle application data binding

---
+++ femtosecond/AppViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace femtosecond
+{
+    public class AppViewModel : BaseBind
+    {
+        public AppViewModel() { }
+
+        private string editorContents;
+        public string EditorContents
+        {
+            get { return editorContents; }
+            set { SetProperty(ref editorContents, value); }
+        }
+
+        private string workspaceDirectory = null;
+        public string WorkspaceDirectory
+        {
+            get { return workspaceDirectory; }
+            set { SetProperty(ref workspaceDirectory, value); }
+        }
+    }
+}
---
+++ femtosecond/BaseBind.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace femtosecond
+{
+    public class BaseBind : System.ComponentModel.INotifyPropertyChanged
+    {
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+
+        protected bool SetProperty<T>(ref T storage, T value,
+            [System.Runtime.CompilerServices.CallerMemberName] String propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
